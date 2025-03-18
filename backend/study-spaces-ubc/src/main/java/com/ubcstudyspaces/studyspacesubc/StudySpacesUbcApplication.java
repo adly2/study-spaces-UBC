a62b9579ee66f631c4f11f338952ede9f82d21e0
adly2 <adooly10@gmail.com>
2025-03-17 18:10:21 -0700

    Initialized spring boot

---
+++ backend/study-spaces-ubc/src/main/java/com/ubcstudyspaces/studyspacesubc/StudySpacesUbcApplication.java
@@ -0,0 +1,13 @@
+package com.ubcstudyspaces.studyspacesubc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpacesUbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpacesUbcApplication.class, args);
+	}
+
+}
---
+++ backend/study-spaces-ubc/src/test/java/com/ubcstudyspaces/studyspacesubc/StudySpacesUbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubcstudyspaces.studyspacesubc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpacesUbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
